0406ee3c33faa09fedb0666a726bc5c69da8cf30
Richard Unger <richard.unger@nexum.com>
2024-03-13 01:14:29 +0100

    unused code

--- src/semaphore_guard.h
+++ src/semaphore_guard.h
@@ -2,17 +2,17 @@
 
 #include <Arduino.h>
 
-class SemaphoreGuard {
-    public:
-        SemaphoreGuard(SemaphoreHandle_t handle) : handle_{handle} {
-            xSemaphoreTake(handle_, portMAX_DELAY);
-        }
-        ~SemaphoreGuard() {
-            xSemaphoreGive(handle_);
-        }
-        SemaphoreGuard(SemaphoreGuard const&)=delete;
-        SemaphoreGuard& operator=(SemaphoreGuard const&)=delete;
+// class SemaphoreGuard {
+//     public:
+//         SemaphoreGuard(SemaphoreHandle_t handle) : handle_{handle} {
+//             xSemaphoreTake(handle_, portMAX_DELAY);
+//         }
+//         ~SemaphoreGuard() {
+//             xSemaphoreGive(handle_);
+//         }
+//         SemaphoreGuard(SemaphoreGuard const&)=delete;
+//         SemaphoreGuard& operator=(SemaphoreGuard const&)=delete;
 
-    private:
-        SemaphoreHandle_t handle_;
-};
+//     private:
+//         SemaphoreHandle_t handle_;
+// };
